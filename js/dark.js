83bc8dede04520d0f2790e3c5616bd682a6a19b3
Rennan <rennandamiao@gmail.com>
2021-04-16 20:23:45 -0300

    a

--- js/dark.js
+++ js/dark.js
@@ -5,13 +5,14 @@ function altera() {
     document.getElementsByClassName("corpo")[0].classList.remove("dark");
     document.getElementsByTagName("body")[0].classList.remove("dark");
     document.getElementsByClassName("porta-botoes")[0].classList.remove("dark");
-   
+    document.getElementsByClassName("secundaryButton")[0].classList.remove("dark");
     estado = false;
   } else {
     //fica escuro
     document.getElementsByTagName("body")[0].classList.add("dark");
     document.getElementsByClassName("porta-botoes")[0].classList.add("dark");
     document.getElementsByClassName("corpo")[0].classList.add("dark");
+    document.getElementsByClassName("secundaryButton")[0].classList.add("dark");
     estado = true;
   }
 }
