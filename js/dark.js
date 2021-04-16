4355908303f8d8a5f7c911dcef4740cc54e68728
Rennan <rennandamiao@gmail.com>
2021-04-16 18:17:32 -0300

    modo dark

---
+++ js/dark.js
@@ -0,0 +1,17 @@
+var estado = false;
+function altera() {
+  if (estado) {
+    //fica claro
+    document.getElementsByClassName("corpo")[0].classList.remove("dark");
+    document.getElementsByTagName("body")[0].classList.remove("dark");
+    document.getElementsByClassName("porta-botoes")[0].classList.remove("dark");
+   
+    estado = false;
+  } else {
+    //fica escuro
+    document.getElementsByTagName("body")[0].classList.add("dark");
+    document.getElementsByClassName("porta-botoes")[0].classList.add("dark");
+    document.getElementsByClassName("corpo")[0].classList.add("dark");
+    estado = true;
+  }
+}
